56d45f9aec3db66a948af395dbbe4a4e87c75177
DamieFC <69485115+DamieFC@users.noreply.github.com>
2020-10-24 10:07:55 -0700

    Add files via upload
    prompt commands

---
+++ commands/prompt and commands.c
@@ -0,0 +1,41 @@
+#import <stdio.h>
+	
+main(){
+	
+	// important variables
+	
+	main(){
+		
+		char prompt = 'user@platypusos';
+		
+		commandPrompt()
+			
+		// this next bit will be ALL of the commands
+			
+		if input == login {				// never declared variable
+			
+			char username = 'PTLV'		// Major
+				
+			char password = 'Platypus'	// security
+				
+			printf("Username: \n")
+				
+			printf("Password: \n")
+				
+				if input				// flaw
+				
+		}
+			
+	}
+	// command prompt
+	
+	type commandPrompt() {
+		
+		while(1) { // This will make it just put the command prompt on screen forever, which would be a majorly bad 					glitch. Must change ASAP, but not sure how
+			printf("%c \n", prompt);
+			
+			if // something = poweroff						 // It's because I'm not sure how to get user input and 			put it on screen. That's the holdup.
+				
+				// code for powering off the computer
+	}
+}
